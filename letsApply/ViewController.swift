be81528feaed5aa8e4d92e6c78426ffde4074fb3
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-11-09 19:17:05 +0200

    Initial Commit

---
+++ letsApply/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  letsApply
+//
+//  Created by Reuben Simphiwe Kuse on 2024/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
